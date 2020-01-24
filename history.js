079bb0da17fe19e7e2cf8b0f244fa9973a34d079
Kirk Scheibelhut <kjs@google.com>
2020-01-23 16:54:56 -0800

    Complicated algorithm for weighting

--- history.js
+++ history.js
@@ -1,85 +1,130 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
+
 const Game = require('./js/game').Game;
+const Stats = require('./js/stats').Stats;
 const Trie = require('./js/trie').Trie
 
 const DICT = require('./data/dict.json');
 const TRIE = Trie.create(DICT);
-const STATS = require('./data/stats.json');
 const HISTORY = require('./history.json');
+const STATS = require('./data/stats.json');
+const stats = new Stats(STATS, DICT);
 
-const possible = {};
-const found = {};
-
+const games = new Map();
 for (const json of HISTORY) {
   const game = Game.fromJSON(json, TRIE, DICT, STATS);
-  const valid= new Set();
+  const valid = new Set();
 
-  const stats = {invalid: 0, total: 0, score: 0};
+  let score = 0;
   let last = game.start;
   for (const w in game.played) {
-    stats.total++;
     const t = game.played[w];
     last = Math.abs(t);
-    if (t < 0) {
-      stats.invalid++;
-    } else {
-      found[w] = (found[w] || 0) + 1;
-      stats.score += Game.score(w);
+    if (t > 0) {
+      score += Game.score(w);
       valid.add(w);
     }
   }
 
-  if (stats.score < 10 ||
-    (stats.score / json.goal.D < 0.25) ||
+  if (score < 10 ||
+    (score / json.goal.D < 0.25) ||
     (last - game.start < 90 * 1000)) {
     continue;
   }
 
-  const anagrams = {};
+  games.set(game, valid);
+}
+
+const ratio = {};
+const anadromes = {};
+const anagrams = {};
+
+const possible = {};
+const found = {};
+let n = games.size;
+for (const [game, valid] of games.entries()) {
+  const as = {};
   for (const w in game.possible) {
     possible[w] = (possible[w] || 0) + 1;
-    const anagram = w.split('').sort().join('');
-    anagrams[anagram] = anagrams[anagram] || [];
-    anagrams[anagram].push(w);
-  }
-}
+    const a = Stats.toAnagram(w);
+    as[a] = as[a] || [];
+    as[a].push(w);
 
-const anadromes = {};
-const ratio = {};
-for (const w in possible) {
-  ratio[w] = DICT[w].n * (1 - ((found[w] || 0) / possible[w]));
+    if (valid.has(w)) {
+      found[w] = (found[w] || 0) + 1;
 
-  const r = w.split('').reverse().join('');
-  if (r === w) continue;
-  if (possible[r]) {
-    const k = [w, r].sort()[0];
-    if ((found[r] || 0) > (found[w] || 0)) {
-      anadromes[k] = DICT[k].n * ((found[r] || 0) - (found[w] || 0));
+      const r = w.split('').reverse().join('');
+      if (r !== w && game.possible[r] && !valid.has(r)) {
+        const k = [w, r].sort()[0];
+        anadromes[k] = (anadromes[w] || 0) + (1 / n * DICT[k].n);
+      }
     } else {
-      anadromes[k] = DICT[k].n * ((found[w] || 0) - (found[r] || 0));
+      ratio[w] = (ratio[w] || 0) + (1 / n * DICT[w].n);
     }
   }
+
+  // TODO depends on how much you already get!
+  for (const a in as) {
+    const group = as[a];
+    if (group.length <= 1) continue;
+    const f = group.filter(w => valid.has(w)).length / group.length;
+    if (!f) continue;
+    const w = group.reduce((acc, w) => acc + DICT[w].n, 0) / group.length;
+    anagrams[a] = (anagrams[a] || 0) + (1 / n * w * (1 - f));
+  }
+  n--;
 }
 
+const K = Math.log(games.size);
+for (const w in possible) {
+  ratio[w] += K * DICT[w].n * Math.pow(1 - ((found[w] || 0) / possible[w]), 2);
+  if (anadromes[w]) {
+    const r = w.split('').reverse().join('');
+    const [n, d] = (found[r] || 0) > (found[w] || 0) ? [w, r] : [r, w];
+    anadromes[w] += K * DICT[w].n * 2 * Math.pow(1 - ((found[n] || 0) / (possible[d] || 1)), 2);
+  }
+
+  const a = Stats.toAnagram(w);
+  if (anagrams[a] && possible[w]) {
+    anagrams[w] += K * DICT[w].n * Math.pow(1 - ((found[w] || 0) / possible[w]), 2);
+  }
+}
+
+
+let out = fs.createWriteStream('words');
 for (const e of Object.entries(ratio).sort((a, b) => b[1] - a[1])) {
   if (found[e[0]] === possible[e[0]]) break;
-  console.log(`${e[0]} ${found[e[0]] || 0}/${possible[e[0]]}`);
+  out.write(`${e[0]} ${found[e[0]] || 0}/${possible[e[0]]}\n`);
 }
+out.close();
 
-console.log('---');
 
-
+out = fs.createWriteStream('anadromes');
 for (const e of Object.entries(anadromes).sort((a, b) => b[1] - a[1])) {
   if (!e[1]) break;
 
   const k = e[0];
   const r = k.split('').reverse().join('');
-  if ((found[r] || 0) > (found[k] || 0)) {
-    console.log(`${k} ${found[k] || 0}/${found[r] || 0} ${r} (${DICT[k].n})`);
-  } else {
-    console.log(`${r} ${found[r] || 0}/${found[k] || 0} ${k} (${DICT[k].n})`);
-  }
+  const [n, d] = (found[r] || 0) > (found[k] || 0) ? [k, r] : [r, k];
+
+  out.write(`${n} ${found[n] || 0}/${found[d] || 0} ${d}\n`);
 }
+out.close();
+
+out = fs.createWriteStream('anagrams');
+for (const e of Object.entries(anagrams).sort((a, b) => b[1] - a[1])) {
+  if (!e[1]) break;
+
+  let s = [];
+  for (const w of stats.anagrams[e[0]]) {
+    if (!possible[w]) continue;
+    s.push(`${w} ${found[w] || 0}/${possible[w] || 0}`);
+  }
+
+  if (s.length > 1) out.write(`${s.join(' ')}\n`);
+}
+out.close();
 
