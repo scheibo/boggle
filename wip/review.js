e229453ecea1c64e0812dcfc73fc765251478df8
Kirk Scheibelhut <kjs@scheibo.com>
2020-02-07 19:00:04 -0800

    Implement review

---
+++ wip/review.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict';
+
+const define = require('../js/dict').define;
+const DICT = require('../data/dict.json');
+const STATS = require('../data/stats.json');
+const Stats = require('../js/stats').Stats;
+const stats = new Stats(STATS, DICT);
+
+const data = require('./data.json');
+const keys = data
+  .filter(w => w.e < 2.5)
+  .sort((a, b) => a.e - b.e)
+  .map(w => w.k);
+
+for (const k of keys) {
+  let defns = [];
+  for (const r of order(stats.anagrams(k, 'New').words)) {
+    const w = r.replace(/[^A-Z]/, '');
+    defns.push(`${r} ${define(w, DICT)}`);
+  }
+  console.log(`${defns.join('\n')}\n`);
+}
+
+function order(words) {
+  const ordered = [];
+
+  const anadromes = new Set();
+  for (const w of words) {
+    const r = w
+      .split('')
+      .reverse()
+      .join('');
+    if (r !== w && words.includes(r)) {
+      const key = `${[w, r].sort().join(' ')}`;
+      if (!anadromes.has(key)) {
+        anadromes.add(key);
+        ordered.push(`(${w}`, `${r})`);
+      }
+    } else {
+      ordered.push(w);
+    }
+  }
+  return ordered;
+}
