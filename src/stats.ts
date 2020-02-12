e84cdaf62390ea44d69d5d646affa46c1f346571
Kirk Scheibelhut <kjs@scheibo.com>
2020-02-12 13:26:45 -0800

    prettier

--- src/stats.ts
+++ src/stats.ts
@@ -124,10 +124,10 @@ export class Stats {
           const r = reverse(w);
           if (r !== w && possible[r] && !played.has(r)) {
             const k = [w, r].sort()[0];
-            anadromes[k] = (anadromes[w] || 0) + (1 / n * this.dict[k][d]!);
+            anadromes[k] = (anadromes[w] || 0) + (1 / n) * this.dict[k][d]!;
           }
         } else {
-          ratio[w] = (ratio[w] || 0) + (1 / n * this.dict[w][d]!);
+          ratio[w] = (ratio[w] || 0) + (1 / n) * this.dict[w][d]!;
         }
       }
 
