e96cdbe9f27423bd315574e496a16386d7e1f676
Kirk Scheibelhut <kjs@scheibo.com>
2020-02-17 16:38:49 -0800

    Clean

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// https://github.com/parcel-bundler/parcel/issues/1762
-import 'regenerator-runtime/runtime';
-import {UI} from './ui/ui';
-// tslint:disable-next-line:no-floating-promises
-UI.create();
